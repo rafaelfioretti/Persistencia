258f8a3c6291eb89f8b05b140e7859109f4b698d
rafaelfioretti <rafaelfioretti@hotmail.com>
2016-12-12 22:10:06 -0200

    ajustes

--- app/src/main/java/br/com/rafaelfioretti/persistencia/MinhaAplicacao.java
+++ app/src/main/java/br/com/rafaelfioretti/persistencia/MinhaAplicacao.java
@@ -5,6 +5,9 @@ import android.content.Context;
 
 import com.facebook.stetho.Stetho;
 
+import br.com.rafaelfioretti.persistencia.dao.DBHelper;
+import br.com.rafaelfioretti.persistencia.dao.DatabaseManager;
+
 /**
  * Created by rafaelfioretti on 12/12/16.
  */
@@ -14,6 +17,7 @@ public class MinhaAplicacao extends Application {
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
+        DatabaseManager.initializeInstance(new DBHelper());
     }
 
     public static Context getContext(){
--- app/src/main/java/br/com/rafaelfioretti/persistencia/dao/LivroDAO.java
+++ app/src/main/java/br/com/rafaelfioretti/persistencia/dao/LivroDAO.java
@@ -1,5 +1,12 @@
 package br.com.rafaelfioretti.persistencia.dao;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.rafaelfioretti.persistencia.model.Livro;
 
 /**
@@ -7,6 +14,8 @@ import br.com.rafaelfioretti.persistencia.model.Livro;
  */
 
 public class LivroDAO {
+    
+    private SQLiteDatabase db;
 
     public static String createTable() {
         return "CREATE TABLE " + Livro.NOME_TABELA + " ( "
@@ -15,4 +24,88 @@ public class LivroDAO {
                 + Livro.AUTOR + " text, "
                 + Livro.EDITORA + " text)";
     }
+
+    public String insereDado(Livro livro) {
+        ContentValues valores;
+        long resultado;
+
+        db = DatabaseManager.getInstance().openDatabase(false);
+        valores = new ContentValues();
+        valores.put(Livro.TITULO, livro.getTitulo());
+        valores.put(Livro.AUTOR, livro.getAutor());
+        valores.put(Livro.EDITORA, livro.getEditora());
+        resultado = db.insert(Livro.NOME_TABELA, null, valores);
+
+        DatabaseManager.getInstance().closeDatabase();
+
+        if (resultado == -1)
+            return "Erro ao inserir registro";
+        else
+            return "Registro Inserido com sucesso";
+    }
+
+    public List<Livro> carregaDados() {
+        List<Livro> livros = new ArrayList<>();
+
+        Cursor cursor;
+        String[] campos = {Livro.ID, Livro.TITULO};
+        db = DatabaseManager.getInstance().openDatabase(true);
+        //cursor = db.query(banco.TABELA, campos, null, null, null, null, null, null);
+        cursor = db.query(Livro.NOME_TABELA, null, null, null, null, null, null, null);
+
+        if (cursor != null) {
+            if(cursor.moveToFirst()){
+                do {
+                    Livro livro = new Livro();
+                    livro.setId(cursor.getInt(cursor.getColumnIndex(Livro.ID)));
+                    livro.setEditora(cursor.getString(cursor.getColumnIndex(Livro.EDITORA)));
+                    livro.setTitulo(cursor.getString(cursor.getColumnIndex(Livro.TITULO)));
+                    livro.setAutor(cursor.getString(cursor.getColumnIndex(Livro.EDITORA)));
+                    livros.add(livro);
+                } while(cursor.moveToNext());
+            }
+        }
+        DatabaseManager.getInstance().closeDatabase();
+        return livros;
+    }
+
+    public Cursor carregaDadoById(int id){
+        Cursor cursor;
+        String[] campos =  {Livro.ID,Livro.TITULO,Livro.AUTOR,Livro.EDITORA};
+        String where = Livro.ID + "=" + id;
+        db = DatabaseManager.getInstance().openDatabase(true);
+        cursor = db.query(Livro.NOME_TABELA, campos, where, null, null, null, null, null);
+
+        if(cursor!=null){
+            cursor.moveToFirst();
+        }
+        DatabaseManager.getInstance().closeDatabase();
+        return cursor;
+    }
+
+    public void alteraRegistro(Livro livro){
+        ContentValues valores;
+        String where;
+
+        db = DatabaseManager.getInstance().openDatabase(false);
+
+        where = Livro.ID + "=" + livro.getId();
+
+        valores = new ContentValues();
+        valores.put(Livro.TITULO, livro.getTitulo());
+        valores.put(Livro.AUTOR, livro.getAutor());
+        valores.put(Livro.EDITORA, livro.getEditora());
+
+        db.update(Livro.NOME_TABELA, valores,where,null);
+        DatabaseManager.getInstance().closeDatabase();
+    }
+
+    public void deletaRegistro(int id){
+        String where = Livro.ID + "=" + id;
+        db = DatabaseManager.getInstance().openDatabase(false);
+        db.delete(Livro.NOME_TABELA,where,null);
+        DatabaseManager.getInstance().closeDatabase();
+    } 
+    
+    
 }
